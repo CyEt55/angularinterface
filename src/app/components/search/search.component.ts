eb11dabf75122048e1f5bf2f94438cfa5864dd14
CyEt55 <albertz_2011@hotmail.com>
2023-06-02 14:43:49 -0500

    step 03_03 incomplete search bar

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html'
 })
 export class SearchComponent implements OnInit {
+  @Output() queryEvt = new EventEmitter<string>();
+
   constructor() {}
 
   ngOnInit() {}
+
+  handleQuery(query: string){
+    this.queryEvt.emit(query);
+  }
 }
